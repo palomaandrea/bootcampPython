eafc753d2ddd6df6c498c2f98355aa3d0c4cc80a
Paloma Andrea <pal.contreras@duocuc.cl>
2024-05-30 23:04:57 -0400

    Segundo ejercicio this

--- 1_fundamentos-de-la-web/js/this/script.js
+++ 1_fundamentos-de-la-web/js/this/script.js
@@ -1,8 +1,10 @@
-// Obtenemos el botón por su id
-var botonCambiar = document.getElementById("botonCambiar");
+// Obtenemos todas las imágenes con la clase 'imagenEliminar'
+var imagenes = document.querySelectorAll(".imagenEliminar");
 
-// Agregamos un evento 'click' al botón
-botonCambiar.addEventListener("click", function () {
-    // Cambiamos el contenido del botón utilizando this y innerText
-    this.innerText = "¡Texto cambiado!";
+// Agregamos un evento 'click' a cada imagen
+imagenes.forEach(function (imagen) {
+    imagen.addEventListener("click", function () {
+        // Eliminamos la imagen haciendo referencia a 'this'
+        this.remove();
+    });
 });
